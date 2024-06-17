f09ec8f0d98d9d2c6d8f1e1d4cbb39710a2d42ea
ASLAMKURESHI <125907818+ASLAMKURESHI@users.noreply.github.com>
2024-06-17 18:00:50 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 12453 
+var accountEmail = "aslam@2038google.com"
+let accountPassword = 786786
+accountCity = "delhi"
+let accountState ; // undefined
+
+// accountId = 9876
+console.table([accountId , accountEmail , accountPassword ,accountCity , accountState])
